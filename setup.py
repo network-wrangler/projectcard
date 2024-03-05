f3ce7274697d0ff513c8601779bc65356b6e14d8
Elizabeth Sall <easall@gmail.com>
2024-03-05 10:35:16 -0800

    add back setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="projectcard",
+    version="0.0.1",
+    description="",
+    packages=find_packages(include=['projectcard']),
+    author="Elizabeth Sall",
+    install_requires=[
+        "jsonschema"
+    ],
+    scripts=["bin/validate_card"],
+)
